1a349c4b0e4083176862d3b4bb173ed67fa06d68
Piyush <piyushprasad816@gmail.com>
2022-10-30 19:03:38 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="border">
       <div className="container text-center py-5 text-gray-700">
-        Create by{" "}
+        Created by{" "}
         <a
           href="https://devocoe.in"
           className="hover:underline"
